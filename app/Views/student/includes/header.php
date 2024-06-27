81fd3d8aecd2a1292a55e18b47b990271778684b
Sayan996 <sanubro01@gmail.com>
2024-06-28 01:14:31 +0530

    update position

--- app/Views/student/includes/header.php
+++ app/Views/student/includes/header.php
@@ -9,7 +9,7 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="<?=base_url()?>/assets/css/style.css?v=1.0.1" rel="stylesheet">
+    <link href="<?=base_url()?>/assets/css/style.css?v=1.0.2" rel="stylesheet">
     <!-- bootstarp CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- bootstrap font -->
