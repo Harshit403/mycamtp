d537c32f805e41588ba8139c49f75f92ccb362c9
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-06-28 00:03:27 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -1,56 +1,53 @@
 <?= $this->extend('student/layout/layout') ?>
 <?=$this->section('content')?>
 
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="margin-top: 79px;">
+
+<div id="carouselExampleCrossfade" class="carousel slide carousel-fade" data-mdb-ride="carousel" data-mdb-carousel-init>
+  <div class="carousel-indicators">
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCrossfade"
+      data-mdb-slide-to="0"
+      class="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCrossfade"
+      data-mdb-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselExampleCrossfade"
+      data-mdb-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="<?=base_url()?>carousel/assets/image1.jpg" class="d-block w-100 imgClass" alt="...">
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="<?=base_url()?>carousel/assets/image1.jpg" class="d-block w-100 imgClass" alt="...">
+    <div class="carousel-item active">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" class="d-block w-100" alt="Wild Landscape"/>
     </div>
     <div class="carousel-item">
-      <img src="<?=base_url()?>carousel/assets/image1.jpg" class="d-block w-100 imgClass" alt="...">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" class="d-block w-100" alt="Camera"/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" class="d-block w-100" alt="Exotic Fruits"/>
     </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
-<style>
-  .carousel-inner {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 
-  .imgClass {
-    width: 100%;
-    height: auto;
-    max-height: 700px;
-    object-fit: cover;
-    aspect-ratio: 1024 / 700;
-  }
 
-  @media (min-width: 1200px) {
-    .imgClass {
-      height: 700px;
-      width: auto;
-    }
-  }
-
-  @media (min-width: 768px) and (max-width: 1199.98px) {
-    .imgClass {
-      height: 700px;
-      width: auto;
-    }
-  }
-
-  @media (max-width: 767.98px) {
-    .imgClass {
-      height: 700px;
-      width: auto;
-    }
-  }
-</style>
 
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
