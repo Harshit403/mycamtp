b1bb4ea31f5b77b8d59805afd1c6c150ac3055ad
Sayan996 <sanubro01@gmail.com>
2024-06-30 01:12:23 +0530

    menu button slightly udpated and in responsice logo update in drawer

--- app/Views/student/includes/header-section.php
+++ app/Views/student/includes/header-section.php
@@ -30,12 +30,16 @@
               </ul>
             </div>
         <?php endif ?>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" style="margin-left: -4rem!important;border: none;">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" style="margin-left: -4rem!important;border: none;margin-right: -1rem;">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">MYCAMTP</h5>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+              <a href="<?=base_url()?>">
+                <img src="<?=base_url()?>/assetItems/image/logo.jpg" style="height:3rem;">
+              </a>
+            </h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
