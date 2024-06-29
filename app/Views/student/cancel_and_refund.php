40e7bc1c4431a02bb1036cba4f9b6e0fbf2dfe63
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-06-29 17:59:33 +0530

    Update cancel_and_refund.php

--- app/Views/student/cancel_and_refund.php
+++ app/Views/student/cancel_and_refund.php
@@ -2,23 +2,7 @@
 <?=$this->section('content')?>
 	<section class="container subject_container" style="padding-top: 5rem;">
 		<h2 class="text-center">Cancel And Refund Policy</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
-		<div class="row">
-			<div class="col-md-12">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</div>
+		Thank you for being a part of MY CS MTP. We do not issue refunds for any product or service once the order is confirmed and delivered/availed and the payment is made. We recommend contacting us for assistance if you experience any issues receiving any services or products.
 		</div>
 	</section>
 <?= $this->endSection() ?>
