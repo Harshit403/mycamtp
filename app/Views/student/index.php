a9b7af8d9425846abd051cb3ddd389d866389707
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-07-01 00:46:52 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -384,7 +384,6 @@
         </div>
 
 
-
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
         <div class="text-center unique-newsletter-box">
             <h6 class="unique-text-primary mb-2">NEWSLETTER</h6>
@@ -414,10 +413,6 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-      
-
-
 
       
     </section>
