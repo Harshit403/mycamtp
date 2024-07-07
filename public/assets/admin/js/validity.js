f9ad60e324f9d7b706702fcb0d77a257147a9a72
Sayan996 <sanubro01@gmail.com>
2024-07-07 19:05:03 +0530

    update new

--- public/assets/admin/js/validity.js
+++ public/assets/admin/js/validity.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-    console.log('hellop');
     $(".typeValidityBtn").on('click', function() {
+        var html = '<div class="row">' + ''
+        '<div class="col-md-12"><input type="text" id="admin_password" class=></div>' +
+        '</div>';
+        bootbox.dialog({
+            messeage:
+        })
         var type_id = $(this).data('type-id');
         $.ajax({
             url: baseUrl + 'admin/close-validity',
