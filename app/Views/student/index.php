a987169cfcba7b29ae78c5e5aabb8156ecdf5e0f
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-05 20:12:18 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -9,6 +9,76 @@
 <!-- enter your meta keywords widthout tag -->
 <?=$this->endSection()?>
 <?=$this->section('content')?>
+
+<style>
+    .button-85 {
+        padding: 0.6em 2em;
+        border: none;
+        outline: none;
+        color: rgb(255, 255, 255);
+        background: #111;
+        cursor: pointer;
+        position: relative;
+        z-index: 1;
+        border-radius: 10px;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        text-align: center;
+    }
+
+    .button-85:before {
+        content: "";
+        background: linear-gradient(
+            45deg,
+            #ff0000,
+            #ff7300,
+            #fffb00,
+            #48ff00,
+            #00ffd5,
+            #002bff,
+            #7a00ff,
+            #ff00c8,
+            #ff0000
+        );
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        background-size: 400%;
+        z-index: -1;
+        filter: blur(5px);
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        animation: glowing-button-85 20s linear infinite;
+        transition: opacity 0.3s ease-in-out;
+        border-radius: 10px;
+    }
+
+    @keyframes glowing-button-85 {
+        0% {
+            background-position: 0 0;
+        }
+        50% {
+            background-position: 400% 0;
+        }
+        100% {
+            background-position: 0 0;
+        }
+    }
+
+    .button-85:after {
+        z-index: -1;
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: #222;
+        left: 0;
+        top: 0;
+        border-radius: 10px;
+    }
+  </style>
+
  <!-- <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="margin-top: 79px;">
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="5000">
@@ -39,7 +109,7 @@
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
-    <section class="py-5 category container" id="category-section">
+   <!-- <section class="py-5 category container" id="category-section">
         <h1 class="text-uppercase text-center">Category</h1>
         <div class="d-flex align-items-center justify-content-center flex-wrap" style="gap:10px 20px;">
           <?php if (!empty($categoryDetails)): ?>
@@ -50,7 +120,22 @@
               <?php endforeach ?>
           <?php endif ?>
         </div>
-    </section>
+    </section> -->
+
+<section class="py-5 category container" id="category-section">
+    <h1 class="text-uppercase text-center">Category</h1>
+    <div class="d-flex align-items-center justify-content-center flex-wrap" style="gap:10px 20px;">
+      <?php if (!empty($categoryDetails)): ?>
+          <?php foreach ($categoryDetails as $categoryRow): ?>
+              <a class="strached-link" href="<?=base_url()?>level?category=<?=$categoryRow->category_short_name?>" style="text-decoration: none;">
+                  <button class="button-85" role="button"><?=$categoryRow->category_name?></button>
+              </a>
+          <?php endforeach ?>
+      <?php endif ?>
+    </div>
+</section>
+
+
     <section class="py-5 container" id="content-section">
         <h1 class="text-center text-uppercase">Our Goals</h1>
         <div class="row">
