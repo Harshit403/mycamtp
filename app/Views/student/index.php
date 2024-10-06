1aa5fdba89a131396b035c470cb81a884d7c7960
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-06 05:53:46 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -42,12 +42,12 @@
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section> -->
-
+<div>
   <section class="container text-center my-5 py-5 bg-white">
-        <h1 class="display-4 text-dark mb-4">MISSION CS <span class="text-success">Test Series</span></h1>
+        <h1 class="display-4 text-dark mb-4">MISSION CS <span class="text-dark">Test Series</span></h1>
         <p class="lead text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
-
+</div>
    <!-- <section class="py-5 category container" id="category-section">
         <h1 class="text-uppercase text-center">Category</h1>
         <div class="d-flex align-items-center justify-content-center flex-wrap" style="gap:10px 20px;">
