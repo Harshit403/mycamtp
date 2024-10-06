3cdff1e9f35b39a4f65c44ea73f0f335cfc633f6
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-06 21:15:52 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -275,13 +275,10 @@
 <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
+      <img class="d-block w-100" src="<?=base_url()?>/assetItems/image/cr1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img class="d-block w-100" src="<?=base_url()?>/assetItems/image/cr2.jpg" alt="Second slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
