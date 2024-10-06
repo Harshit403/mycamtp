33e8af4b1986ee943158de42cbca4b25db0e95bc
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-06 21:12:12 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -268,9 +268,39 @@
                   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.3</div>
                 </div>
             </div>
-            <div class="col-md-4">
+         <!--   <div class="col-md-4">
                 <img src="<?=base_url()?>/assetItems/image/image2.jpg" class="img-thumbnail" style="width: 400px;">
-            </div>
+            </div> -->
+<div class="col-md-4">
+<div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+<style>
+.carousel-item {
+  transition: opacity 0.5s ease-in-out; /* Adjust duration and easing as needed */
+} 
+</style>
+
+
         </div>
     </section>
     <section class="py-5 container" id="faq-section">
