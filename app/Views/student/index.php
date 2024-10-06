5b46f35bae58a3696e2f09625e5e47f3e720f49a
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-06 05:48:00 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -43,8 +43,8 @@
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section> -->
 
-  <section class="container text-center my-5 py-5 bg-white shadow rounded">
-        <h1 class="display-4 text-success mb-4">The Key of <span class="text-success">Success</span></h1>
+  <section class="container text-center my-5 py-5 bg-white">
+        <h1 class="display-4 text-success mb-4">MISSION CS <span class="text-success">Test Series</span></h1>
         <p class="lead text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </section>
 
