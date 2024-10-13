a34aab27db3777d68497b1142c826f790e77000d
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-13 06:25:03 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -21,6 +21,13 @@
 <?=$this->endSection()?>
 <?=$this->section('content')?>
 
+<div itemscope itemtype="https://schema.org/WebSite">
+  <link itemprop="url" href="https://missioncstestseries.com" />
+    <meta itemprop="name" content="Mission CS Test Series"/>
+    <meta itemprop="alternateName" content="CS Test Series"/>
+</div>
+
+
  <!-- <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="margin-top: 79px;">
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="5000">
