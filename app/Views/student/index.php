50c18cab3028a0ef19df03b2e815342b4036232f
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-21 14:19:52 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -903,38 +903,6 @@ At Mission CS Test Series, we have designed each question papers considering the
           </div>
         <?php endif ?>
     </section>
- <!-- <section class="py-5 container" id="testimonial-screen">
-        <!-- <h1 class="text-uppercase text-center">TESTIMONIALS</h1> -->
-      <h1-not>Testimonials</h1-not>
-        <div>
-          <div id="carouselExampleDark" class="carousel carousel-dark slide">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="10000">
-                <img src="<?=base_url()?>assetItems/image/image1.jpg" class="d-block w-100" alt="..." style="height: 300px;">
-              </div>
-              <div class="carousel-item" data-bs-interval="2000">
-                <img src="<?=base_url()?>assetItems/image/image2.jpg" class="d-block w-100" alt="..." style="height: 300px;">
-              </div>
-              <div class="carousel-item">
-                <img src="<?=base_url()?>assetItems/image/image3.jpg" class="d-block w-100" alt="..." style="height: 300px;">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
-        </div>
-    </section> -->
 
 <section>
    <!-- Card Section -->
