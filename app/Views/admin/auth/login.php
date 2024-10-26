f51c7e0bf032c51ffe472122eb0378505c35ac8c
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-10-26 19:17:31 +0530

    Update login.php

--- app/Views/admin/auth/login.php
+++ app/Views/admin/auth/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/cdn/css/bootstrap.min.css">
