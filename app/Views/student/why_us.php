3cfba821db73b75cc5866444b15495914cebfee9
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-11-20 19:35:41 +0530

    Update why_us.php

--- app/Views/student/why_us.php
+++ app/Views/student/why_us.php
@@ -13,9 +13,16 @@
         <div class="text-center">
             <h2>Why Us</h2>
         </div>
-
-
-
+        <div class="whyUsContainer py-3">
+            <div class="row" style="min-height:50vh !important">
+                <div class="col-md-12 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            </div>
+        </div>
     </section>
 <?= $this->endSection() ?>
 <?= $this->section('jsContent') ?>
