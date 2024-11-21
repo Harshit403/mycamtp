1ba7c69f830477d460cd63a7c9a9e0c00b0c9d0f
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-11-21 12:03:13 +0530

    Update header-section.php

--- app/Views/student/includes/header-section.php
+++ app/Views/student/includes/header-section.php
@@ -300,7 +300,7 @@ content="Boost Your CS Exam Success with Our Comprehensive Test Series. Prepare 
     </button>
 
     <!-- Offcanvas Drawer -->
-    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="height: 60vh;">
+    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="height: 53vh;">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
           <a href="<?=base_url()?>">
