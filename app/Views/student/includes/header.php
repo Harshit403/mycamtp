1fab597b8128625009b49f339d44d34d402ef961
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-12-08 15:20:23 +0530

    Update header.php

--- app/Views/student/includes/header.php
+++ app/Views/student/includes/header.php
@@ -10,7 +10,7 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="<?=base_url()?>/assets/css/style.css?v=1.0.1" rel="stylesheet">
+    <link href="<?=base_url()?>/assets/css/style.css?v=1.0.2" rel="stylesheet">
     <!-- bootstarp CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- bootstrap font -->
@@ -20,20 +20,12 @@
     <!-- FontAwesome for icons -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" crossorigin="anonymous"></script>
 
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
    <link rel="manifest" href="<?=base_url()?>/manifest.json">
    <meta name="theme-color" content="#1a73e8">
   
-    <link rel="manifest" href="/manifest.json">
+   <link rel="manifest" href="/manifest.json">
    <meta name="theme-color" content="#1a73e8">
     
-    <script type="application/ld+json">
-  {
-    "@context": "https://schema.org",
-    "@type": "WebSite",
-    "name": "Mission CS Test Series",
-    "url": "https://missioncstestseries.com",
-    "description": "CS Test Series"
-  }
-</script>
 </head>
