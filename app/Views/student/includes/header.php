5ff0d0bc299cea73a22c6ae1dd0876589666962c
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-12-23 00:04:57 +0530

    Update header.php

--- app/Views/student/includes/header.php
+++ app/Views/student/includes/header.php
@@ -8,14 +8,14 @@
     <!-- Favicons -->
     <link href="<?=base_url()?>assets/img/favicon.png" rel="icon">
     <!-- Google Fonts -->
-    <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> 
      <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@277&display=swap" rel="stylesheet">
     <!-- Template Main CSS File -->
     <link href="<?=base_url()?>/assets/css/style.css" rel="stylesheet">
     <!-- bootstarp CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- bootstrap font -->
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome for icons -->
