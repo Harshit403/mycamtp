328ba865a7ddffae655f0191d725d7702684c430
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-12-23 10:31:46 +0530

    Update header-section.php

--- app/Views/student/includes/header-section.php
+++ app/Views/student/includes/header-section.php
@@ -111,7 +111,7 @@
           <li class="nav-item forMobileSection">
             <a class="nav-link showCartBtn" href="javascript:void(0)" class="btn">
               <div style="position: relative;">
-                <i class="fa-solid fa-cart-shopping" style="font-size: 18px;"></i>
+                <i class="fa-solid fa-cart-shopping" style="font-size: 18px; color: black;"></i>
                 <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary cartCount">0</span>
               </div>
             </a>
@@ -167,7 +167,7 @@
                   <li class="nav-item forDesktopSection">
                     <a class="nav-link showCartBtn" href="javascript:void(0)" class="btn">
                       <div style="position: relative;">
-                        <i class="fa-solid fa-cart-shopping" style="font-size: 18px;"></i>
+                        <i class="fa-solid fa-cart-shopping" style="font-size: 18px; color: black;"></i>
                         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary cartCount">0</span>
                       </div>
                     </a>
