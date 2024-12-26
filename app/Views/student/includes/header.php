20c7ae3581b695ac69f1dcbe22ffee611d9ff8dc
Harshit Shrivastav <hshrivastav100@gmail.com>
2024-12-26 14:01:31 +0530

    Update header.php

--- app/Views/student/includes/header.php
+++ app/Views/student/includes/header.php
@@ -21,6 +21,8 @@
     <!-- FontAwesome for icons -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" crossorigin="anonymous"></script>
 
+    <script src="https://cdn.tailwindcss.com"></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
    <link rel="manifest" href="<?=base_url()?>/manifest.json">
