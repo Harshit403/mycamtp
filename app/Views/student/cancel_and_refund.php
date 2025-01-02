b850c8a2627813628671a64e5be996d339758b1a
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-02 21:35:34 +0530

    Update cancel_and_refund.php

--- app/Views/student/cancel_and_refund.php
+++ app/Views/student/cancel_and_refund.php
@@ -11,7 +11,7 @@
 <?=$this->section('content')?>
 	<section class="container subject_container mainContainer">
 		<h2 class="text-center">Cancel And Refund Policy</h2>
-		Thank you for being a part of MY CA MTP. We do not issue refunds for any product or service once the order is confirmed and delivered/availed and the payment is made. We recommend contacting us for assistance if you experience any issues receiving any services or products.
+		Thank you for being a part of Missioncstestseries.com. We do not issue refunds for any product or service once the order is confirmed and delivered/availed and the payment is made. We recommend contacting us for assistance if you experience any issues receiving any services or products.
 		</div>
 	</section>
 <?= $this->endSection() ?>
