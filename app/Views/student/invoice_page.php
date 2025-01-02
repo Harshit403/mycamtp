200b95651f1ef73d167cb5480ee522b885a0c6b0
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-02 19:14:07 +0530

    Update invoice_page.php

--- app/Views/student/invoice_page.php
+++ app/Views/student/invoice_page.php
@@ -19,14 +19,14 @@
                             if (!empty($invoiceJson)) {
                                 $jsonDecoded = json_decode($invoiceJson);
                                 ?>
-                                <div class="col-md-12" style="border: 1px solid;border-radius: 5px;">
+                                <div class="col-md-12 mb-3" style="border: 1px solid;border-radius: 5px;">
                                     <div class="d-flex justify-content-between">
                                         <?php $order_id = $jsonDecoded[0]->order_id; ?>
                                         <?php $payement_date = $jsonDecoded[0]->payement_date; ?>
                                         <h5><?=$order_id?><br/><?=$payement_date?></h5>
                                         <div class="d-flex" style="flex-direction:column;">
-                                            <?=$jsonDecoded[0]->payment_status?>
-                                            <a href="<?=base_url()?>download-invoice?order_id=<?=$order_id?>" class="btn btn-sm btn-success"><i class="bi bi-arrow-down-circle"></i> Download</a>
+                                            <pre><?=$jsonDecoded[0]->payment_status?></pre>
+                                            <a href="<?=base_url()?>download-invoice?order_id=<?=$order_id?>" class="btn btn-sm btn-success mb-3 "><i class="bi bi-arrow-down-circle"></i> Download</a>
                                         </div>
                                     </div>
                                 </div>
