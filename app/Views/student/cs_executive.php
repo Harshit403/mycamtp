f9b60ac5315fa2c2dc5185216f3d373ba19aaab4
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-02 20:22:00 +0530

    Create cs_executive.php

---
+++ app/Views/student/cs_executive.php
@@ -0,0 +1,35 @@
+<?= $this->extend('student/layout/layout') ?>
+<?=$this->section('title')?>
+	CS Executive
+<?=$this->endSection()?>
+<?= $this->section('meta_description') ?>
+<!-- enter your meta description -->
+<?=$this->endSection()?>
+<?= $this->section('meta_keywords') ?>
+<!-- enter your meta keywords widthout tag -->
+<?=$this->endSection()?>
+<?=$this->section('content')?>
+	<section class="container executive_container mainContainer">
+		<h2 class="text-center">CS Executive</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<div class="row">
+			<div class="col-md-12">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+	</section>
+<?= $this->endSection() ?>
+<?=$this->section('jsContent')?>
+<?= $this->endSection() ?>
