2f2dcafe6dac1e321e0caebfcf9fd53d95e2d585
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-02 22:01:43 +0530

    Update gplan.php

--- app/Views/student/gplan.php
+++ app/Views/student/gplan.php
@@ -1,6 +1,6 @@
 <?= $this->extend('student/layout/layout') ?>
 <?=$this->section('title')?>
-	G Plan
+	Pass Guaranteed" Plan-Terms
 <?=$this->endSection()?>
 <?= $this->section('meta_description') ?>
 <!-- enter your meta description -->
@@ -9,26 +9,24 @@
 <!-- enter your meta keywords widthout tag -->
 <?=$this->endSection()?>
 <?=$this->section('content')?>
-	<section class="container G_Plan_container mainContainer">
-		<h2 class="text-center">G Plan</h2>
+	<section class="container G_Plan_container mainContainer" style="margin-top: 8rem;">
+		<h2 class="text-center">"Pass Guaranteed" Plan-Terms</h2>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		   <h1>Guaranteed Pass Plan - Terms and Conditions</h1>
+    <ul>
+        <li>The offer is applicable only if a student appears and writes all the papers of a group at a particular level under the “Guaranteed Pass Plan” scheme.</li>
+        <li>The student must score a minimum of 55 marks in each test paper on the scheduled date.</li>
+        <li>If the student scores less than 55% in even one test, they will not be eligible for a refund.</li>
+        <li>If the student has taken a subscription for one group or both groups, they must secure a minimum of 55% in each paper in our test series.</li>
+        <li>The credentials for checking results must be provided to us within 7 days of the declaration of results.</li>
+        <li>The student must submit certified answer sheets of their main exams within 28 days of the declaration of results.</li>
+        <li>You will be ineligible to claim a refund if you use any promo code on this plan.</li>
+        <li>For further inquiries, you may write to us at <a href="mailto:support@missioncstestseries.com">support@missioncstestseries.com</a>.</li>
+        <li>Mission CS Test Series reserves the right to change the above-mentioned conditions, which will be informed to you in advance and applied prospectively.</li>
+        <li>All the above conditions must be satisfied to claim a refund under this plan.</li>
+        <li>The refund will be provided in the original payment mode and source account or UPI ID.</li>
+    </ul>
 		</p>
-		<div class="row">
-			<div class="col-md-12">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</div>
-		</div>
 	</section>
 <?= $this->endSection() ?>
 <?=$this->section('jsContent')?>
