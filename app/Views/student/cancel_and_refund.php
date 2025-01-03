7297e5cb34fb1c3636c819053582578c5a7d7c81
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-03 12:56:00 +0530

    Update cancel_and_refund.php

--- app/Views/student/cancel_and_refund.php
+++ app/Views/student/cancel_and_refund.php
@@ -9,7 +9,7 @@
 <!-- enter your meta keywords widthout tag -->
 <?=$this->endSection()?>
 <?=$this->section('content')?>
-	<section class="container subject_container mainContainer">
+	<section class="container subject_container mainContainer" style="margin-top: 6rem;>
 		<h2 class="text-center">Cancel And Refund Policy</h2>
 		Thank you for being a part of Missioncstestseries.com. We do not issue refunds for any product or service once the order is confirmed and delivered/availed and the payment is made. We recommend contacting us for assistance if you experience any issues receiving any services or products.
 		</div>
