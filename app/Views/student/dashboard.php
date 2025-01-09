89dafce8bf63623af763fd04e20691dae422a6a0
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-10 00:07:53 +0530

    Update dashboard.php

--- app/Views/student/dashboard.php
+++ app/Views/student/dashboard.php
@@ -633,6 +633,14 @@
     #logoutModal .modal-content {
         max-width: 400px;
       }
+    .referral-section {
+      margin: 15px auto;
+      max-width: auto;
+      background: white;
+      border-radius: 10px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+      padding: 15px;
+          }
 }
       
     .buyNowModal {
