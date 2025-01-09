cc9002b056730266fe6358f2c62b9b24d06fa3ca
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-09 11:50:29 +0530

    Update header.php

--- app/Views/student/includes/header.php
+++ app/Views/student/includes/header.php
@@ -20,13 +20,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome for icons -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" crossorigin="anonymous"></script>
-    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
-   <link rel="manifest" href="<?=base_url()?>/manifest.json">
-   <meta name="theme-color" content="#ffffff">
-  
-   <link rel="manifest" href="/manifest.json">
-   <meta name="theme-color" content="#ffffff">
-    
-</head>
+    <link rel="manifest" href="<?=base_url()?>/manifest.json">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#ffffff">
+    <script type="application/ld+json">
+     {
+     "@context": "https://schema.org",
+     "@type": "WebPage",
+     "name": "Mission CS Test Series",
+     "url": "https://missioncstestseries.com"
+     }
+     </script>
+     </head>
