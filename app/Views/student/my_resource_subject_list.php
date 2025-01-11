538b22914d3a15402445230bee78dc6c88b50b7a
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-11 15:22:57 +0530

    Update my_resource_subject_list.php

--- app/Views/student/my_resource_subject_list.php
+++ app/Views/student/my_resource_subject_list.php
@@ -121,7 +121,7 @@ cs test series, cstestseries, cs executive test series, cs professional test ser
                 <i class="fas fa-lock animated-icon"></i>
             </div>
             <h3 class="buy-plan-title">No Subjects Available</h3>
-            <a class="btn btn-modern" href="<?=base_url()?>level/level-list">Buy any plan</a>
+            <a class="btn btn-modern" href="<?=base_url()?>buy">Buy any plan</a>
         </div>
            </li>
                     </ul>
