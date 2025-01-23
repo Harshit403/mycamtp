0e03c88baabe040bda6baba69ded1c9c2da1c4fc
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-23 15:13:58 +0530

    Update cs_test_series.php

--- app/Views/student/cs_test_series.php
+++ app/Views/student/cs_test_series.php
@@ -8,12 +8,31 @@ BEST TEST SERIES FOR CSEET , CS EXECUTIVE AND CS PROFESSIONAL
 <?= $this->section('meta_keywords') ?>
 CS TEST SERIS, CS EXECUTIVE TEST SERIES, CSEET TEST SERIES, CS PROFESSIONAL TEST SERIES, ICSI TEST SERIES, ICSI PRE-TEST, FREE CS TEST SERIES, TEST SERIES FOR CS EXECUTIVE, TEST SERIES FOR CS PROFESSIONAL, CS MOCK TEST SERIES, CS EXAM TEST SERIES, FREE TEST SERIES, AFFORDABLE TEST SERIES, BEST TEST SERIES, CS EXAMS, ICSI EXAMS, CS EXECUTIVE NEW SYLLABUS TEST SERIES, CS EXECUTIVE FREE TEST SERIES, CS LECTURE, CS EXECUTIVE LECTURES, CS EXECUTIVE NOTES, CS MOCK TEST, MISSION CS TEST SERIES
 <?=$this->endSection()?>
+<meta name="author" content="Mission CS Test Series">
+<meta name="robots" content="index, follow">
+<link rel="canonical" href="https://missioncstestseries.com/">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:title" content="CS Test Series - Mission CS Test Series">
+<meta property="og:description" content="CS Test Series for CSET, CS EXECUTIVE AND CS PROFESSIONAL">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://missioncstestseries.com/">
 <?=$this->section('content')?>
-	<section class="container contact_us_container mainContainer" style="margin-top: 6rem;">
-<div style="margin-top: 18rem;">
-<p>Rom Rom Bhaiyo</p>
+<section class="container contact_us_container mainContainer" style="margin-top: 6rem;">
+<div style="margin-top: 10rem;">
+
+	
 </div>	
-	</section>
+</section>
 <?= $this->endSection() ?>
 <?=$this->section('jsContent')?>
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": "CS TEST SERIES",
+        "description": "CS Test Series for CSEET, CS EXECUTIVE AND CS PROFESSIONAL",
+        "url": "https://missioncstestseries.com/"
+    }
+    </script>
 <?= $this->endSection() ?>
