8a71a367302ea20ad0e3dae22ab7841d913f27a8
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-01-26 17:20:46 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -1099,7 +1099,10 @@ This ensures  that  you Prepare  Smartly  not blindly</p>
     });
   </script>
 
+<!-- start webpushr code --> <script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key':'BKyPm_qNFX-rSaSabH_2G-ub8FGSHPXSGzzM23zaP8NIm2N_rskcfDEVZ7qrSnOdO3sAWLPfMZeYTlbxjZBp1VQ' });</script><!-- end webpushr code -->
 
+
+	
 
 <?= $this->endSection() ?>
 <?=$this->section('jsContent')?>
