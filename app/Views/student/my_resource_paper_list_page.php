0cec84ca78b0757d566423a9e483b7f2fe5ec154
Harish <harish@codefire.in>
2025-02-16 15:49:36 +0530

    paper list

--- app/Views/student/my_resource_paper_list_page.php
+++ app/Views/student/my_resource_paper_list_page.php
@@ -15,92 +15,64 @@
         </div>
 
         <div class="row my-4 paperContainer">
-            <div class="col-md-12 text-center">
-                <div class="container" style="overflow-x: auto;">
-                <table class="table table-striped" id="paper_table">
-                    <thead>
-                        <tr>
-                            <th>Paper Name</th>
-                            <th>Download Question paper</th>
-                            <th>Download Answer paper</th>
-                            <?php if ($item_type!='free'): ?>
-                                <th>Upload Assignment</th>
-                            <?php endif ?>
-                        <tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            if(!empty($getPaperDetails)){
-                                foreach($getPaperDetails as $paperRow){
-                                    $paper_name = str_replace('','_',trim($paperRow['paper_name']));
-                                    ?>
-                                        <tr>
-                                            <td><?=$paperRow['paper_name']?></td>
-                                            <td>    
-                                                <a href="<?=base_url().$paperRow['question_paper_upload']?>" class="btn btn-sm btn-success" download="<?=$paper_name?>question_paper.pdf"><i class="bi bi-arrow-down-circle"></i> Download</a>
-                                            </td>
-                                            <td> 
-                                                <div id="answerBtnContainer<?=$paperRow['paper_id']?>" class="answerBtnContainerClass" style="display:<?=$item_type!='free' ? 'none' : ''?>?;">
-                                                    <a href="<?=base_url().$paperRow['answer_paper_upload']?>" class="btn btn-sm btn-success" download="<?=$paper_name?>answer_paper.pdf"><i class="bi bi-arrow-down-circle"></i> Download</a>
-                                                </div>   
-                                            </td>
-                                            <?php if ($item_type!='free'): ?>
-                                                <td>  
-                                                    <div class="row" id="assignmentConatianer<?=$paperRow['paper_id']?>">
-                                                        <div class="col-md-8">
-                                                            <input type="file" class="form-control mr-2" id="assignmentFile<?=$paperRow['paper_id']?>" accept="application/pdf,image/*"/>
-                                                        </div>
-                                                        <div class="col-md-4">
-                                                            <a href="javascript:void(0)" class="btn btn-info btn-sm uploadAssignment" data-paper-id="<?=$paperRow['paper_id']?>"><i class="bi bi-file-arrow-up"></i></a></a>
-                                                        </div>
-                                                    </div>  
-                                                </td>
-                                            <?php endif ?>
-                                        </tr>
-                                    <?php
-                                }
-                            } else {
-                                ?>
-                                    <tr>
-                                        <td colspan="4">No Paper Available</td>
-                                    </tr>
-                                <?php
-                            }
-                        ?>
-                    </tbody>
-                </table>
+            <?php if(!empty($getPaperDetails)): ?>
+                <?php foreach($getPaperDetails as $paperRow): ?>
+                    <div class="col-md-4 mb-4">
+                        <div class="card shadow-sm border rounded p-3">
+                            <div class="card-body">
+                                <h5 class="card-title text-center font-weight-bold"><?= $paperRow['paper_name'] ?></h5>
+                                <hr>
+                                <div class="d-flex justify-content-between">
+                                    <a href="<?= base_url().$paperRow['question_paper_upload'] ?>" class="btn btn-success btn-sm w-100 m-1" download="<?= str_replace(' ', '_', trim($paperRow['paper_name'])) ?>_question_paper.pdf">
+                                        <i class="bi bi-arrow-down-circle"></i> Question Paper
+                                    </a>
+                                    <div id="answerBtnContainer<?= $paperRow['paper_id'] ?>" class="answerBtnContainerClass" style="display:<?= $item_type!='free' ? 'none' : '' ?>;">
+                                        <a href="<?= base_url().$paperRow['answer_paper_upload'] ?>" class="btn btn-success btn-sm w-100 m-1" download="<?= str_replace(' ', '_', trim($paperRow['paper_name'])) ?>_answer_paper.pdf">
+                                            <i class="bi bi-arrow-down-circle"></i> Answer Paper
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <?php if ($item_type!='free'): ?>
+                                    <hr>
+                                    <div class="upload-container">
+                                        <input type="file" class="form-control mb-2" id="assignmentFile<?= $paperRow['paper_id'] ?>" accept="application/pdf,image/*"/>
+                                        <a href="javascript:void(0)" class="btn btn-info btn-sm uploadAssignment w-100" data-paper-id="<?= $paperRow['paper_id'] ?>">
+                                            <i class="bi bi-file-arrow-up"></i> Upload Assignment
+                                        </a>
+                                        <div id="assignmentConatianer<?= $paperRow['paper_id'] ?>" class="mt-2"></div>
+                                    </div>
+                                <?php endif ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div class="col-12 text-center">
+                    <p>No Paper Available</p>
                 </div>
-            </div>
+            <?php endif; ?>
         </div>
     </section>
 <?= $this->endSection() ?>
-<?= $this->section('jsContent')?>
-    <script>
-        var pageType = 'paper-list';
-    </script>
-    <script> 
 
+<?= $this->section('jsContent')?>
+<script>
 $(document).ready(function() {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     $(".uploadAssignment").on('click', function() {
         var paper_id = $(this).data('paper-id');
-        var assignmentFilePath = $("#assignmentFile" + paper_id + "").val();
+        var assignmentFilePath = $("#assignmentFile" + paper_id).val();
         if (assignmentFilePath == '') {
-            bootbox.alert("Please select your assginment file");
+            bootbox.alert("Please select your assignment file");
             return false;
         }
         let dialog = bootbox.dialog({
-            message: 'Are you sure you want to submit assignment',
+            message: 'Are you sure you want to submit assignment?',
             size: 'medium',
-            onEscape: true,
-            backdrop: true,
             buttons: {
                 no: {
                     label: 'No',
-                    className: 'btn-secondary',
-                    callback: function() {
-
-                    }
+                    className: 'btn-secondary'
                 },
                 yes: {
                     label: 'Yes',
@@ -108,114 +80,37 @@ $(document).ready(function() {
                     callback: function() {
                         uploadAssignmentFile(paper_id);
                     }
-                },
+                }
             }
-        })
+        });
     });
 
-    function uploadAssignmentFile(paper_id = '') {
+    function uploadAssignmentFile(paper_id) {
         var formdata = new FormData();
-        var errors = new Array;
-        var assginmentFile = $("#assignmentFile" + paper_id + "").prop('files')[0];
-        var extension = assginmentFile['name'].split(".");
-        if (jQuery.inArray(extension[1], ['pdf', 'jpg', 'jpeg', 'png']) == -1) {
-            errors.push('Please select a image or pdf file to upload');
-            return false;
-        }
+        var assignmentFile = $("#assignmentFile" + paper_id).prop('files')[0];
         formdata.append('paper_id', paper_id);
-        formdata.append('assignment_file', assginmentFile);
+        formdata.append('assignment_file', assignmentFile);
+        
         $.ajax({
             url: baseUrl + 'upload/assignment-file',
             type: 'POST',
             data: formdata,
-            cache: false,
             processData: false,
             contentType: false,
             dataType: 'json',
             beforeSend: function() {
-                html = '<div class="col-md-12 text-warning font-weight-bold d-flex align-items-center justify-content-center">Upload In Progress <i class="fa-solid fa-spin fa-spinner ml-2"></i></div>';
-                $("#assignmentConatianer" + paper_id + "").html(html);
+                $("#assignmentConatianer" + paper_id).html('<div class="text-warning font-weight-bold">Uploading...</div>');
             },
             success: function(response) {
                 if (response.success) {
-                    $("#assignmentConatianer" + paper_id + "").html('');
-                    uploadAssigmentStatus();
+                    $("#assignmentConatianer" + paper_id).html('<div class="text-success font-weight-bold">Uploaded Successfully!</div>');
                 }
             },
-            error: function(xhr, ajaxOptions, thrownError) {
-                html = '<div class="col-md-12 text-danger font-weight-bold d-flex align-items-center justify-content-center">Upload Process Failed<br>(File size limit exceed) Use max 30mb file <i class="fas fa-times ml-2"></i></div>';
-                $("#assignmentConatianer" + paper_id + "").html(html);
+            error: function() {
+                $("#assignmentConatianer" + paper_id).html('<div class="text-danger font-weight-bold">Upload Failed! Max file size: 30MB.</div>');
             }
         });
     }
-
-    if (pageType != null && pageType == 'paper-list') {
-        uploadAssigmentStatus();
-        setInterval(function() {
-            uploadAssigmentStatus();
-        }, 20000);
-    }
-
-
-    function uploadAssigmentStatus() {
-        $.ajax({
-            url: baseUrl + 'fetch-assignment-status',
-            type: 'GET',
-            dataType: 'json',
-            success: function(response) {
-                if (response.length > 0) {
-                    var assignmentStatus = 'Assignment Submitted';
-                    var statusColor = 'text-warning';
-                    $.each(response, function(i, v) {
-                        if (v.assignment_status == 2) {
-                            html = '<div class="col-md-12 text-center"><a href="' + baseUrl + v.assignment_checked_file + '" class="btn btn-success" download title="Download Checked AnswerSheet"><i class="fas fa-download"></i> Download</a></div>';
-                            $("#answerBtnContainer" + v.paper_id + "").show();
-                        } else if (v.assignment_status == 1) {
-                            html = '<div class="col-md-12 ' + statusColor + ' font-weight-bold d-flex align-items-center justify-content-center">' + assignmentStatus + '</div>';
-                            $("#answerBtnContainer" + v.paper_id + "").show();
-                        } else {
-                            html = '<div class="col-md-8">' +
-                                '<input type="file" class="form-control mr-2" id="assignmentFile' + response.paper_id + '" accept="application/pdf,image/*"/>' +
-                                '</div>' +
-                                '<div class="col-md-4">' +
-                                '<a href="javascript:void(0)" class="btn btn-info uploadAssignment" data-paper-id="' + response.paper_id + '"><i class="fas fa-upload"></i></a></a>' +
-                                '</div>';
-                        }
-                        $("#assignmentConatianer" + v.paper_id + "").html(html);
-                    });
-                } else {
-                    $(".answerBtnContainerClass").hide();
-                }
-            }
-        })
-    }
-
-    $(".addNewsLetterBtn").on('click', function() {
-        var email = $(".newsLetterForm").val();
-        if (!regex.test(email)) {
-            bootbox.alert('Please enter a valid mail');
-            return false;
-        }
-
-        $.ajax({
-            url: baseUrl + 'newsletter/add-newsletter',
-            type: 'POST',
-            data: {
-                newsletter_email: email,
-            },
-            dataType: 'json',
-            success: function(res) {
-                bootbox.alert({
-                    message: res.message,
-                    closeButton: false,
-                    callback: function() {
-                        if (res.success) {}
-                    }
-                })
-            }
-        })
-
-    })
-})
-    </script>
+});
+</script>
 <?= $this->endSection() ?>
