2885c98f39b454b74f3d6a6b1e708a200fc78bd9
Harshit Shrivastav <hshrivastav100@gmail.com>
2025-02-27 11:53:26 +0530

    Update student_auth.php

--- app/Views/student/auth/student_auth.php
+++ app/Views/student/auth/student_auth.php
@@ -89,8 +89,8 @@
                 <label>
   <input type="checkbox" id="agree" onchange="toggleButton()"> 
   I have read and agree to the 
-  <a href="terms.html" target="_blank">Terms and Conditions</a> and 
-  <a href="privacy.html" target="_blank">Privacy Policy</a>.
+  <a href="<?=base_url()?>terms-and-condition" target="_blank">Terms and Conditions</a> and 
+  <a href="<?=base_url()?>privacy-policy" target="_blank">Privacy Policy</a>.
                 </label>
             </form>
             <a class="btn btn-sm btn-custom signUpBtn authButton  w-100" id="signupBtn" disabled style="background-color: gray; cursor: not-allowed;>
