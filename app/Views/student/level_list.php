c73e64d0a7a96d14f116972723b68644731f8d69
Harshit <hshrivastav100@gmail.com>
2025-04-15 16:51:39 +0530

    Update level_list.php

--- app/Views/student/level_list.php
+++ app/Views/student/level_list.php
@@ -18,9 +18,10 @@
     }
     .level-btn {
         display: block; 
-        margin: 0.3rem; 
+        margin: 0.5rem auto; 
         background-color: #e63e58; 
-        width: 88%; 
+        max-width: 300px;
+        width: 100%;
         color: #fff; 
         font-size: 14px; 
         font-weight: bold; 
@@ -33,15 +34,43 @@
         text-decoration: none;
         background-color: #c9344d;
     }
+    .level-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .level-col {
+        flex: 0 0 100%;
+        max-width: 100%;
+        padding: 0 15px;
+    }
+    @media (min-width: 576px) {
+        .level-col {
+            flex: 0 0 50%;
+            max-width: 50%;
+        }
+    }
+    @media (min-width: 768px) {
+        .level-col {
+            flex: 0 0 33.333%;
+            max-width: 33.333%;
+        }
+    }
+    @media (min-width: 992px) {
+        .level-col {
+            flex: 0 0 25%;
+            max-width: 25%;
+        }
+    }
     </style>
         <div class="d-flex justify-content-center" style="margin-top: 10rem;">
             <div class="col-md-12">
                 <h2 class="text-center"><?=$category_info->category_more_details_heading?></h2>
             </div>
         </div>
-        <div class="row">
+        <div class="level-container">
             <?php foreach ($level_list as $levelRow): ?>
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div class="level-col">
                     <a href="<?=base_url()?>type?category=<?=$levelRow->category_short_name?>&&level=<?=$levelRow->level_short_name?>" class="level-btn">
                         <?=$levelRow->level_name?>
                         <i class="fas fa-arrow-right" style="margin-left: 10px; animation: slideRight 1s infinite;"></i>
