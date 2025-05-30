78311d96b306d38f92d589ba717ba0803693d8ea
Harshit <hshrivastav100@gmail.com>
2025-05-30 11:03:02 +0530

    Update index.php

--- app/Views/student/index.php
+++ app/Views/student/index.php
@@ -610,9 +610,6 @@ cs test series, cstestseries , cs executive test series, cs professional test se
     </div>
 </section>
 
-
-
-
     <!-- Newsletter Section -->
     <section class="mcs-newsletter mcs-section" data-aos="fade-up">
         <div class="mcs-container">
@@ -620,7 +617,7 @@ cs test series, cstestseries , cs executive test series, cs professional test se
             <p>Subscribe to our newsletter and stay updated with the latest preparation strategies and exclusive discounts</p>
             <form class="mcs-newsletter-form">
                 <input class="newsLetterForm" type="email" placeholder="Enter your email address" required>
-               <button type="submit" class="addNewsLetterBtn">Subscribe</button>
+               <button type="submit" class="addNewsLetterBtn" onclick="event.preventDefault(); return false;">Subscribe</button>
             </form>
         </div>
     </section>
