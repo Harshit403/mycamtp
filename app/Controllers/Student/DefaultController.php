8a5441c3f6380009b82fd36986c79e223811c7f4
Harish123540 <hbhatt41@gmail.com>
2025-07-06 22:30:08 +0530

    Update DefaultController.php

--- app/Controllers/Student/DefaultController.php
+++ app/Controllers/Student/DefaultController.php
@@ -507,7 +507,12 @@ class DefaultController extends BaseController
 			session()->set('billingDetails', $updateData);
 		}
 
+		
 		$linkInfo = $this->cashfreePayment($studentDetails, $total_amt_to_pay, $order_id);
+$linkInfo = json_decode($linkInfo);
+echo '<pre>';
+print_r($linkInfo);
+exit;
 		if (!empty($linkInfo)) {
 			$linkInfo = json_decode($linkInfo);
 			session()->set('link_id', isset($linkInfo->order_id) ? $linkInfo->order_id : '');
